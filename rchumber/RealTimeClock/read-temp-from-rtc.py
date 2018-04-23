1f3f1fcbb1d80b9c1888be134490bba54dc36620
rchumber <ramanchumber1@gmail.com>
2018-04-22 22:48:34 -0700

    DS3231 Real time clock test/debug code

---
+++ rchumber/RealTimeClock/read-temp-from-rtc.py
@@ -0,0 +1,37 @@
+## python
+
+import smbus
+import os
+import time
+
+# Release RTC 3231
+os.system('sudo rmmod rtc_ds1307')
+
+# Setup RTC 3231 for temperature reading
+bus = smbus.SMBus(1)
+address = 0x68
+CONV = 32
+
+# Force a conversion and wait until it completes
+def convTemp(address):
+    byte_control = bus.read_byte_data(address,0x0E)
+    if byte_control&CONV == 0:
+        bus.write_byte_data(address, 0x0E, byte_control|CONV)
+    byte_control = bus.read_byte_data(address,0x0E)
+    while byte_control&CONV != 0:
+        time.sleep(1)
+        byte_control = bus.read_byte_data(address,0x0E)
+    return True
+
+# Get temperature in degrees C
+def getTemp(address):
+    convTemp(address)
+    byte_tmsb = bus.read_byte_data(address,0x11)
+    byte_tlsb = bus.read_byte_data(address,0x12)
+    tinteger = (byte_tmsb & 0x7f) + ((byte_tmsb & 0x80) >> 7) * -2**8
+    tdecimal = (byte_tmsb >> 7) * 2**(-1) + ((byte_tmsb & 0x40) >> 6) * 2**(-2)
+    return tinteger + tdecimal
+
+Celsius = getTemp(address)
+Fahrenheit = 9.0/5.0 * Celsius + 32
+print Fahrenheit, "*F /", Celsius, "*C"
