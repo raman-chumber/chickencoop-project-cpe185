87e66b08a4e38b8c4ccdadad4123a52356b76771
rchumber <ramanchumber1@gmail.com>
2018-04-29 11:57:36 -0700

    Temperature from DS3231 in combined file

--- rchumber/combined/allsensors.py
+++ rchumber/combined/allsensors.py
@@ -19,6 +19,13 @@ import smbus
 import os
 import time
 
+# Release RTC 3231
+os.system('sudo rmmod rtc_ds1307')
+
+# Setup RTC 3231 for temperature reading
+bus = smbus.SMBus(1)
+address = 0x68
+CONV = 32
 
 #constants
 SHUNT_OHMS = 0.1
@@ -42,14 +49,42 @@ def readvoltage():
 		print("Bus Current: %.3f mA" % ina.current())
 		print("Power: %.3f mW" % ina.power())
 		print("Shunt voltage: %.3f mV" % ina.shunt_voltage())
-		print("\n")
 	except DeviceRangeError as e:
 		# Current out of device range with specified shunt resister
 		print(e)
 	return True
 
+# Force a conversion and wait until it completes
+def convTemp(address):
+    byte_control = bus.read_byte_data(address,0x0E)
+    if byte_control&CONV == 0:
+        bus.write_byte_data(address, 0x0E, byte_control|CONV)
+    byte_control = bus.read_byte_data(address,0x0E)
+    while byte_control&CONV != 0:
+        time.sleep(1)
+        byte_control = bus.read_byte_data(address,0x0E)
+    return True
+
+# Get temperature in degrees C
+def getTemp(address):
+    convTemp(address)
+    byte_tmsb = bus.read_byte_data(address,0x11)
+    byte_tlsb = bus.read_byte_data(address,0x12)
+    tinteger = (byte_tmsb & 0x7f) + ((byte_tmsb & 0x80) >> 7) * -2**8
+    tdecimal = (byte_tmsb >> 7) * 2**(-1) + ((byte_tmsb & 0x40) >> 6) * 2**(-2)
+    return tinteger + tdecimal
+
+def readtemperature():
+	Celsius = getTemp(address)
+	Fahrenheit = 9.0/5.0 * Celsius + 32
+	print ("Fahrenheit: %.1f *F" % Fahrenheit)
+	print ("Celsius: %.1f *C" % Celsius)
+	return True
+
 while True:
 	readlight()
 	readvoltage()
+	readtemperature()
+	print("\n")
 	sleep(0.6)
 	
