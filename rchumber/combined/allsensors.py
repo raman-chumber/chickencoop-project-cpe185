08147e5ca4c899ba3502d950e020678188ab70fd
rchumber <ramanchumber1@gmail.com>
2018-04-29 12:06:10 -0700

    Code cleanup for combined file

--- rchumber/combined/allsensors.py
+++ rchumber/combined/allsensors.py
@@ -3,7 +3,7 @@
 #
 #  allsensors.py
 
-#sensor libraries from Adafruit
+# sensor libraries from Adafruit
 #==============================
 
 #1. light sensor
@@ -27,10 +27,10 @@ bus = smbus.SMBus(1)
 address = 0x68
 CONV = 32
 
-#constants
+# constants
 SHUNT_OHMS = 0.1
 
-
+# Function to Read light sensor data
 def readlight():
 	tsl = tsl2591.Tsl2591()  # initialize
 	full, ir = tsl.get_full_luminosity()  # read raw values (full spectrum and ir spectrum)
@@ -40,6 +40,7 @@ def readlight():
 	print ("Infrared: %.2f" % ir)
 	return True
 
+# Function to read voltage data
 def readvoltage():
 	ina = INA219(SHUNT_OHMS)
 	ina.configure()
@@ -73,7 +74,7 @@ def getTemp(address):
     tinteger = (byte_tmsb & 0x7f) + ((byte_tmsb & 0x80) >> 7) * -2**8
     tdecimal = (byte_tmsb >> 7) * 2**(-1) + ((byte_tmsb & 0x40) >> 6) * 2**(-2)
     return tinteger + tdecimal
-
+# Convert and output temperature data
 def readtemperature():
 	Celsius = getTemp(address)
 	Fahrenheit = 9.0/5.0 * Celsius + 32
@@ -81,10 +82,11 @@ def readtemperature():
 	print ("Celsius: %.1f *C" % Celsius)
 	return True
 
+# loop to call functions one by one
 while True:
-	readlight()
-	readvoltage()
-	readtemperature()
+	readlight() 			# Call light sensor function
+	readvoltage()			# Call voltage sensor 
+	readtemperature()		# Call temperature sensor
 	print("\n")
 	sleep(0.6)
 	
